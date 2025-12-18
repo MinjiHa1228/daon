6327d2163ae6e3303aa1604a46fb2ab4f34e9341
MinjiHa1228 <haminji2047@gmail.com>
2025-12-18 13:14:12 +0900

    init: Spring Boot JSP base working

---
+++ src/main/java/com/example/daon/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.daon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	
+	   
+	   @GetMapping("/test")
+	   public String test() {
+	       return "home";	   
+
+}
+}
